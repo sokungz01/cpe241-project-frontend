c11d50885857c31881b606916fa40219bc026b14
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-04-25 17:15:22 +0700

    chore(styling) | CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "#003150",
+        clike: "#0174BE",
+        normal: "#3490CB",
+        primary: "1890FF",
+        bg: "#E6F1F9",
+      },
+    },
   },
   plugins: [],
-}
+};
