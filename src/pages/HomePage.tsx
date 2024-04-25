972221de9c2c88a1dfdbada4aa14b3fabbe434dd
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-04-25 18:07:40 +0700

    update to test navbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/Login/Navbar";
+
 const HomePage = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-col h-screen">
+      <Navbar />
       <div className="m-auto">
         <p className="text-xl font-bold">Home</p>
       </div>
