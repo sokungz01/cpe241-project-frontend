2e235eb2493eeffa8b55af000787e7fb9276119e
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2024-05-01 11:39:25 +0700

    feat(config) | antd config provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage, NotFoundPage } from "@/pages";
 import LoginPage from "./pages/LoginPage";
+import { ConfigProvider } from "antd";
+import { theme } from "./config/antdTheme";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ConfigProvider theme={theme}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
     </>
   );
 }
